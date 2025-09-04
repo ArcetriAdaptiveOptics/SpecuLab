f496d49de5bd6f51a81feba2f45038399c3fb19f
Alfio Puglisi <alfio.puglisi@gmail.com>
2025-09-04 08:58:32 +0200

    Added normalization functions andmodal base function from SPECULA

--- speculab/if_reduction/pipeline.py
+++ speculab/if_reduction/pipeline.py
@@ -9,7 +9,7 @@ from astropy.convolution import convolve_fft, Gaussian2DKernel
 from skimage.transform import resize
 
 
-from typing import Iterator
+from typing import Iterator, Iterable
 
 
 # From itertools recipes
@@ -37,7 +37,7 @@ def _crop_to_valid(data):
     return data[y_min:y_max+1, x_min:x_max+1]
 
 def load_all_files(path: os.PathLike) -> Iterator:
-    '''Load FITS data from all filenames matching a given path pattern'''
+    '''Load 2D FITS data from all filenames matching a given path pattern'''
     filelist = glob.glob(path)
     if len(filelist) == 0:
         raise FileNotFoundError(path)
@@ -57,6 +57,18 @@ def cube_diff(images: Iterator, preview=False) -> Iterator:
         yield up-down
         if preview: # Only first diff for preview
             break
+
+def normalize_constant(images: Iterator, constant: float) -> Iterator:
+    '''Normalize all images by a constant value'''
+    for image in images:
+        yield image / constant
+
+
+def normalize_to_vector(images: Iterator, value_vector: os.PathLike) -> Iterator:
+    '''Normalize each image by a different value'''
+    norm = fits.getdata(value_vector)
+    for i, image in enumerate(images):
+        yield image / norm[i]
 
 
 def smooth_image(image):
@@ -100,15 +112,58 @@ def crop_and_resize(image, rows: int, cols: int):
 
 
 def stack_images(images: Iterator, save_path: os.PathLike=None, preview=False):
-    '''Stack images into a 3D array'''
-    if preview:
-        stack = np.stack(list(islice(images, 2)))
-    else:
-        stack = np.stack(list(images))
+    '''Stack images into a 3D array and optionally save them'''
+    stack = []
+    for i, image in enumerate(images):
+        stack.append(image)
+        yield image
+        if preview and i == 1:
+            break
+    stack = np.stack(list(stack))
     if save_path:
         fits.writeto(save_path, stack, overwrite=True)
         print(f'Stack saved to {save_path}')    
 
 
+def modal_base(images: Iterator,
+               tel_diameter_in_m: float,
+               r0_in_m: float,
+               L0_in_m: float,
+               zern_modes: int,
+               klbasis_save_path: os.PathLike,
+               m2c_save_path: os.PathLike,
+               s_save_path: os.PathLike,
+               use_cupy=True, single_precision=True):
+    '''Generate modal base'''
+    if use_cupy:
+        try:
+            import cupy as xp
+        except ImportError:
+            print('Cupy not available, falling back to numpy')
+            xp = np
+    else:
+        xp = np
+
+    import specula
+    specula.init(0)
+    from specula import cpuArray
+    from specula.lib.modal_base_generator import make_modal_base_from_ifs_fft
+
+    dtype = xp.float32 if single_precision else xp.float64
+    ifunc = xp.stack(map(xp.array(images))).astype(dtype)
+    mask = (ifunc.sum(axis=0) != 0).astype(dtype)
+    ifunc2d = ifunc[:, mask]
+
+    klbasis, m2c, s = make_modal_base_from_ifs_fft(mask,
+                                                   diameter=tel_diameter_in_m,
+                                                   influence_functions=ifunc2d,
+                                                   r0=r0_in_m,
+                                                   L0=L0_in_m,
+                                                   zern_modes=zern_modes,
+                                                   xp=xp, dtype=dtype)
+
+    fits.writeto(klbasis_save_path, cpuArray(klbasis))
+    fits.writeto(m2c_save_path, cpuArray(m2c))
+    fits.writeto(s_save_path, cpuArray(s))
 
 
